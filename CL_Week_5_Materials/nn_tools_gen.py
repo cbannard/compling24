e46c3fae3735e54b410a9d5d48dfe20785c8c6d3
cbannard <colinbannard@gmail.com>
2025-03-01 13:18:29 +0000

    Add files via upload

---
+++ CL_Week_5_Materials/nn_tools_gen.py
@@ -0,0 +1,188 @@
+"""## Imports"""
+
+from argparse import Namespace
+from collections import Counter
+import json
+import os
+import string
+import numpy as np
+import pandas as pd
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data import Dataset, DataLoader
+from tqdm import tqdm_notebook
+
+
+def make_train_state(args):
+    return {'stop_early': False,
+            'early_stopping_step': 0,
+            'early_stopping_best_val': 1e8,
+            'learning_rate': args.learning_rate,
+            'epoch_index': 0,
+            'train_loss': [],
+            'train_acc': [],
+            'val_loss': [],
+            'val_acc': [],
+            'test_loss': -1,
+            'test_acc': -1,
+            'model_filename': args.model_state_file}
+
+def update_train_state(args, model, train_state):
+    """Handle the training state updates.
+
+    Components:
+     - Early Stopping: Prevent overfitting.
+     - Model Checkpoint: Model is saved if the model is better
+
+    :param args: main arguments
+    :param model: model to train
+    :param train_state: a dictionary representing the training state values
+    :returns:
+        a new train_state
+    """
+    import torch
+    # Save one model at least
+    if train_state['epoch_index'] == 0:
+        torch.save(model.state_dict(), train_state['model_filename'])
+        train_state['stop_early'] = False
+
+    # Save model if performance improved
+    elif train_state['epoch_index'] >= 1:
+        loss_tm1, loss_t = train_state['val_loss'][-2:]
+
+        # If loss worsened
+        if loss_t >= train_state['early_stopping_best_val']:
+            # Update step
+            train_state['early_stopping_step'] += 1
+        # Loss decreased
+        else:
+            # Save the best model
+            if loss_t < train_state['early_stopping_best_val']:
+                torch.save(model.state_dict(), train_state['model_filename'])
+
+            # Reset early stopping step
+            train_state['early_stopping_step'] = 0
+
+        # Stop early ?
+        train_state['stop_early'] = \
+            train_state['early_stopping_step'] >= args.early_stopping_criteria
+
+    return train_state
+
+def normalize_sizes(y_pred, y_true):
+    """Normalize tensor sizes
+    
+    Args:
+        y_pred (torch.Tensor): the output of the model
+            If a 3-dimensional tensor, reshapes to a matrix
+        y_true (torch.Tensor): the target predictions
+            If a matrix, reshapes to be a vector
+    """
+    if len(y_pred.size()) == 3:
+        y_pred = y_pred.contiguous().view(-1, y_pred.size(2))
+    if len(y_true.size()) == 2:
+        y_true = y_true.contiguous().view(-1)
+    return y_pred, y_true
+
+def compute_accuracy(y_pred, y_target):
+    import torch
+    _, y_pred_indices = y_pred.max(dim=1)
+    n_correct = torch.eq(y_pred_indices, y_target).sum().item()
+    return n_correct / len(y_pred_indices) * 100
+
+def compute_accuracy_gen(y_pred, y_true, mask_index):
+    y_pred, y_true = normalize_sizes(y_pred, y_true)
+
+    _, y_pred_indices = y_pred.max(dim=1)
+    
+    correct_indices = torch.eq(y_pred_indices, y_true).float()
+    valid_indices = torch.ne(y_true, mask_index).float()
+    
+    n_correct = (correct_indices * valid_indices).sum().item()
+    n_valid = valid_indices.sum().item()
+
+    return n_correct / n_valid * 100
+
+def set_seed_everywhere(seed, cuda):
+    import torch
+    import numpy as np
+    
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    if cuda:
+        torch.cuda.manual_seed_all(seed)
+
+def sequence_loss(y_pred, y_true, mask_index):
+    y_pred, y_true = normalize_sizes(y_pred, y_true)
+    return F.cross_entropy(y_pred, y_true, ignore_index=mask_index)
+
+        
+def handle_dirs(dirpath):
+    if not os.path.exists(dirpath):
+        os.makedirs(dirpath)
+
+def predict_nationality(surname, classifier, vectorizer):
+    import torch
+    """Predict the nationality from a new surname
+    
+    Args:
+        surname (str): the surname to classifier
+        classifier (SurnameClassifer): an instance of the classifier
+        vectorizer (SurnameVectorizer): the corresponding vectorizer
+    Returns:
+        a dictionary with the most likely nationality and its probability
+    """
+    vectorized_surname = vectorizer.vectorize(surname)
+    vectorized_surname = torch.tensor(vectorized_surname).view(1, -1)
+    result = classifier(vectorized_surname, apply_softmax=True)
+
+    probability_values, indices = result.max(dim=1)
+    index = indices.item()
+
+    predicted_nationality = vectorizer.nationality_vocab.lookup_index(index)
+    probability_value = probability_values.item()
+
+    return {'nationality': predicted_nationality, 'probability': probability_value}
+
+def predict_nationality_rnn(surname, classifier, vectorizer):
+    vectorized_surname, vec_length = vectorizer.vectorize(surname)
+    vectorized_surname = torch.tensor(vectorized_surname).unsqueeze(dim=0)
+    vec_length = torch.tensor([vec_length], dtype=torch.int64)
+    
+    result = classifier(vectorized_surname, vec_length, apply_softmax=True)
+    probability_values, indices = result.max(dim=1)
+    
+    index = indices.item()
+    prob_value = probability_values.item()
+
+    predicted_nationality = vectorizer.nationality_vocab.lookup_index(index)
+
+    return {'nationality': predicted_nationality, 'probability': prob_value, 'surname': surname}
+
+def column_gather(y_out, x_lengths):
+    import torch
+    '''Get a specific vector from each batch datapoint in `y_out`.
+
+    More precisely, iterate over batch row indices, get the vector that's at
+    the position indicated by the corresponding value in `x_lengths` at the row
+    index.
+
+    Args:
+        y_out (torch.FloatTensor, torch.cuda.FloatTensor)
+            shape: (batch, sequence, feature)
+        x_lengths (torch.LongTensor, torch.cuda.LongTensor)
+            shape: (batch,)
+
+    Returns:
+        y_out (torch.FloatTensor, torch.cuda.FloatTensor)
+            shape: (batch, feature)
+    '''
+    x_lengths = x_lengths.long().detach().cpu().numpy() - 1
+
+    out = []
+    for batch_index, column_index in enumerate(x_lengths):
+        out.append(y_out[batch_index, column_index])
+
+    return torch.stack(out)
